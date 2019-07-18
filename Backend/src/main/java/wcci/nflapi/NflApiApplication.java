99632461fb70d77851b17b048ed3dc9408b13cd2
tyler-conner <tyler.conner@gmail.com>
2019-07-18 16:03:03 -0400

    new repo

---
+++ Backend/src/main/java/wcci/nflapi/NflApiApplication.java
@@ -0,0 +1,13 @@
+package wcci.nflapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NflApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NflApiApplication.class, args);
+	}
+
+}
---
+++ Backend/src/test/java/wcci/nflapi/NflApiApplicationTests.java
@@ -0,0 +1,16 @@
+package wcci.nflapi;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class NflApiApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
